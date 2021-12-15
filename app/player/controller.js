fda788ba18255f3aaafb3953f9410201dfbceaad
darwinknowless <id.darwins@gmail.com>
2021-12-16 06:05:06 +0700

    API : Detail Page

--- app/player/controller.js
+++ app/player/controller.js
@@ -13,4 +13,22 @@ module.exports = {
 			res.status(500).json({ message: err.message || 'Server Error' });
 		}
 	},
+
+	detailPage: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const voucher = await Voucher.findOne({ _id: id })
+				.populate('category')
+				.populate('nominals')
+				.populate('user', '_id name phoneNumber');
+
+			if (!voucher) {
+				return res.status(404).json({ message: 'Voucher not found' });
+			}
+
+			res.status(200).json({ data: voucher });
+		} catch (err) {
+			res.status(500).json({ message: err.message || 'Server Error' });
+		}
+	},
 };
