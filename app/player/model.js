abe74f8c03a2ddaf2984462320cf80f24d5f0941
darwinknowless <id.darwins@gmail.com>
2021-12-17 14:38:54 +0700

    API : Signup

--- app/player/model.js
+++ app/player/model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const HASH_ROUNDS = 10;
 
 let playerSchema = mongoose.Schema(
 	{
@@ -15,14 +17,14 @@ let playerSchema = mongoose.Schema(
 		username: {
 			type: String,
 			require: [true, 'Username must be entered'],
-			maxLength: [32, 'Username must be 8 - 32 characters'],
-			minLength: [8, 'Username must be 8 - 32 characters'],
+			maxLength: [32, 'Username must be 7 - 32 characters'],
+			minLength: [7, 'Username must be 7 - 32 characters'],
 		},
 		password: {
 			type: String,
 			require: [true, 'Password must be entered'],
-			maxLength: [32, 'Password must be 8 - 32 characters'],
-			minLength: [8, 'Password must be 8 - 32 characters'],
+			maxLength: [32, 'Password must be 7 - 32 characters'],
+			minLength: [7, 'Password must be 7 - 32 characters'],
 		},
 		role: {
 			type: String,
@@ -53,5 +55,24 @@ let playerSchema = mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+// Validate email already exists/registered
+playerSchema.path('email').validate(
+	async function (value) {
+		try {
+			const count = await this.model('Player').countDocuments({ email: value });
+			return !count;
+		} catch (err) {
+			throw err;
+		}
+	},
+	(attr) => `${attr.value} already exists`
+);
+
+// Encrypt password before saving
+playerSchema.pre('save', function (next) {
+	this.password = bcrypt.hashSync(this.password, HASH_ROUNDS);
+	next();
+});
 
 module.exports = mongoose.model('Player', playerSchema);
